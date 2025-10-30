6f1e71a6113485ed4c53b570ba73756b191d6023
WhiteRave <anastasia20002@yandex.ru>
2025-10-30 13:13:15 +0500

    Удален старый admin-файл

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.config import settings
 from app.database import engine
 from app.models import Base
-from app.routers import auth, user_trading, admin
+from app.routers import auth, user_trading
 from app.routers import api_v1_public, api_v1_balance, api_v1_order, api_v1_admin
 
 app = FastAPI(openapi_url="/openapi.json", docs_url="/docs")
@@ -23,7 +23,6 @@ async def startup():
 # Роутеры
 app.include_router(auth.router)
 app.include_router(user_trading.router)
-app.include_router(admin.router)
 app.include_router(api_v1_public.router)
 app.include_router(api_v1_balance.router)
 app.include_router(api_v1_order.router)
